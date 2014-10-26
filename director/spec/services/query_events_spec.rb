8902204f8ec043e0349cac856d46643ddebff4a8
Timothy Sabat <timothy.sabat@gmail.com>
2014-10-26 08:33:22 -0700

    more tests

--- director/spec/services/query_events_spec.rb
+++ director/spec/services/query_events_spec.rb
@@ -9,20 +9,38 @@ RSpec.describe QueryEvents do
     expect(rslt.length).to eq(1)
   end
 
-  it 'filters time', focus: true do
-    service = QueryEvents.new
+  describe 'general hours' do
+    it 'falls within hours' do
+      service = QueryEvents.new
 
-    date       = Chronic.parse('this sunday 9:00AM')
-    event      = FactoryGirl.create(:event, start_time: date - 1.hour, general_hours: true)
-    open_hour  = FactoryGirl.create(:open_hour,
-                                    event_id: event.id,
-                                    open_hour: '08:00',
-                                    day_of_the_week: 'Sunday')
+      date       = Chronic.parse('this sunday 8:00AM')
+      event      = FactoryGirl.create(:event, start_time: date, general_hours: true)
+      open_hour  = FactoryGirl.create(:open_hour,
+                                      event_id: event.id,
+                                      open_hour: '08:00',
+                                      day_of_the_week: 'Sunday')
 
-    start_time = date.strftime('%Y-%m-%dT%l:%M:%S%z')
+      start_time = date.strftime('%Y-%m-%dT%l:%M:%S%z')
 
-    rslt = service.call({start_time: start_time})
-    expect(rslt.length).to eq(1)
-    expect(rslt.last.id).to eq(event.id)
+      rslt = service.call({start_time: start_time})
+      expect(rslt.length).to eq(1)
+      expect(rslt.last.id).to eq(event.id)
+    end
+
+    it 'is outside hours' do
+      service = QueryEvents.new
+
+      date       = Chronic.parse('this sunday 8:00AM')
+      event      = FactoryGirl.create(:event, start_time: date, general_hours: true)
+      open_hour  = FactoryGirl.create(:open_hour,
+                                      event_id: event.id,
+                                      open_hour: '09:00',
+                                      day_of_the_week: 'Sunday')
+
+      start_time = date.strftime('%Y-%m-%dT%l:%M:%S%z')
+
+      rslt = service.call({start_time: start_time})
+      expect(rslt.length).to eq(0)
+    end
   end
 end
