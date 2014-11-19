879a9e835f30e7035a4108dceec35a40e3ce2c52
Seth Taylor <sethtaylor26@gmail.com>
2014-11-18 17:08:27 -0800

    adding model test for admin_user

--- director/spec/models/admin_user_spec.rb
+++ director/spec/models/admin_user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'checks presense' do
+    user = AdminUser.new
+    user.save
+
+    expect(user.errors.messages[:email]).to_not be nil
+
+    expect(user.persisted?).to eq(false)
+
+  end
 end
