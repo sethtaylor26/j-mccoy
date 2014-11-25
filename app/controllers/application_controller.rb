65768ac35dfb0079090f7b74fbfa435d092da462
Seth Taylor <sethtaylor26@gmail.com>
2014-11-24 17:10:04 -0800

    fixed bug with registration, but broke the generated controllers using devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   
   before_filter :authenticate_user_from_token!
     
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
     
   private
   
